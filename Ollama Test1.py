8e7988fbed568a1c35ddfe362567f5c931300191
BaAydin <aydin.batuhan@hotmail.de>
2025-02-10 16:03:51 +0100

    Update Ollama Test1.py

--- Ollama Test1.py
+++ Ollama Test1.py
@@ -7,23 +7,10 @@ res = ollama.chat(
     messages=[
         {
             "role": "user", 
-            "content": "emoriable World War 2 dates and what happened there in very short?",
+            "content": "How many R's does the word strawberry have?",
         },
     ],
     stream=True,
 )
 for chunk in res:
     print(chunk["message"]["content"], end="", flush=True)
-
-
-# #Create a new model with modelfile
-# modelfile =""""
-# FROM llama3.2
-# SYSTEM You are going to answer very concise with as few words as needed
-# PARAMETER temperature 0.1
-# """
-
-# ollama.create(model="Concise", modelfile=modelfile)
-
-# res = ollama.generate(model="Concise", prompt="memoriable World War 2 dates and what happened there")
-# print(res["response"])
